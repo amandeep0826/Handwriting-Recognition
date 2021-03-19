9666edb21de2ba15f189565d5dd7c7c691ed6964
Amandeep <69617589+amandeep0826@users.noreply.github.com>
2021-03-19 23:51:55 +0530

    Update App.js

--- src/App.js
+++ src/App.js
@@ -1,9 +1,7 @@
 import React from 'react';
 
 import './App.css';
-import Title from './components/Title/Title.js'
 import Canvas from './components/Canvas/Canvas.js'
-import Instructions from './components/Instructions/Instructions.js'
 
 
 function App() {

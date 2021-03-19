266181c6b117401108c46a1810f45c40e652f7aa
Amandeep <69617589+amandeep0826@users.noreply.github.com>
2021-03-19 23:51:22 +0530

    Add files via upload

---
+++ src/App.js
@@ -0,0 +1,17 @@
+import React from 'react';
+
+import './App.css';
+import Title from './components/Title/Title.js'
+import Canvas from './components/Canvas/Canvas.js'
+import Instructions from './components/Instructions/Instructions.js'
+
+
+function App() {
+  return (
+    <div className="App">
+      <Canvas />
+    </div>
+  );
+}
+
+export default App;
